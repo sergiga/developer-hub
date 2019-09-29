9fd9915b7546584f8990005a214b31a1d85d1a90
sergiga <srgarcia91@gmail.com>
2019-09-29 15:18:04 +0200

    Added json formatter initial iteration

--- src/renderer/components/RequestDetailsPage.vue
+++ src/renderer/components/RequestDetailsPage.vue
@@ -1,42 +1,46 @@
 <template>
   <div id="wrapper">
     <main>
-      <VButton
+      <v-button
         color="dark"
-        disabled
         icon="arrow-circle-left"
         link
         @click="navigateToList" />
-      <VButton
-        disabled
-        icon="arrow-circle-left"
-        outline
-        @click="navigateToList" />
-      <VButton
-        disabled
-        icon="arrow-circle-left"
-        link
-        @click="navigateToList" />
-      <VButton
-        disabled
-        icon="arrow-circle-left"
-        @click="navigateToList" />
+      <v-json-formatter v-if="response" :json="response"/>
     </main>
   </div>
 </template>
 
 <script>
 import VButton from '@/components/VButton'
+import VJsonFormatter from '@/components/VJsonFormatter'
+import { mapGetters } from 'vuex'
 import { REQUEST_LIST_PAGE } from '@/router/index'
 
 export default {
   name: 'request-details-page',
   components: {
-    VButton
+    VButton,
+    VJsonFormatter
+  },
+  created () {
+    if (!this.request) {
+      this.navigateToList()
+    }
   },
   methods: {
     navigateToList () {
       this.$router.push({ name: REQUEST_LIST_PAGE })
+    }
+  },
+  computed: {
+    ...mapGetters(['getRequestByID']),
+    request () {
+      return this.getRequestByID(parseInt(this.$route.params.id))
+    },
+    response () {
+      if (!this.request) { return }
+      return this.request.response
     }
   }
 }
---
+++ src/renderer/components/VJsonFormatter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex align-items-center justify-content-start">
+    <unicon
+      v-if="icon"
+      :name="icon"
+      width="1rem"
+      height="1rem"/>
+    <span>{{ raw }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-json-formatter',
+  props: {
+    json: {
+      type: [Object, Array],
+      required: true
+    },
+    root: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    raw () {
+      return JSON.stringify(this.json)
+    }
+  }
+}
+</script>
